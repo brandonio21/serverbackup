73502d7c151ae4c3bad1a90241fa0a871c55b61d
Brandon Milton <brandon.milton21@gmail.com>
2020-11-25 21:55:35 +0000

    Add logging

--- serverbackup.py
+++ serverbackup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import logging
 import json
 from io import BytesIO
 import tarfile
@@ -10,8 +11,18 @@ import time
 BACKUP_ROOT = "/var/backups"
 CONFIG_FILE = "/etc/serverbackup.conf"
 
+# configure logs
+logger = logging.getLogger("serverbackup")
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
+logger.addHandler(console_handler)
+
 
 def main() -> int:
+    logger.debug("Parsing config file")
     with open(CONFIG_FILE, "r") as f:
         config = json.load(f)
     # required fields
@@ -24,11 +35,12 @@ def main() -> int:
 
     timestamp = int(time.time())
     backup_path = f"{BACKUP_ROOT}/serverbackup-{name}-{timestamp}.tar.gz"
+    logger.debug(f"Starting backup of {name} to {backup_path}")
 
     backup = tarfile.open(backup_path, mode="w:gz")
-
     # Step 1: Dump database and add it to the backup tar
     for database, user, password in databases:
+        logger.debug(f"Dumping database {database}")
         dump_proc = subprocess.run(
             ["mysqldump", database, f"--user={user}", f"--password={password}"],
             stdout=subprocess.PIPE,
@@ -40,9 +52,11 @@ def main() -> int:
 
     # Step 2: Add all files in DIRS_TO_BACKUP
     for dir_to_backup in dirs_to_backup:
+        logger.debug(f"Adding directory {dir_to_backup}")
         backup.add(dir_to_backup, recursive=True)
 
     # Step 3: Create a METADATA file
+    logger.debug("Adding METADATA")
     metadata = {}
     metadata["timestamp"] = timestamp
     metadata_data = json.dumps(metadata).encode("utf-8")
@@ -54,6 +68,7 @@ def main() -> int:
 
     # Step 4: If config has data, upload to s3 using s3cmd
     if s3config and s3bucket:
+        logger.debug("Uploading to s3")
         subprocess.run(
             ["s3cmd", "--config", s3config, "put", backup_path, f"s3://{s3bucket}"]
         )
